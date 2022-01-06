feb6d0b6ab1d5dd3e611083c8385e39b0215642e
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-01-07 00:34:25 +0100

    cf_pokedex project
    cf_pokedex project created. "pokemonList" created consisting of 3 different objects ("Pokemon")

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert("Hello world");
-let favoriteFood = "Banana";
-document.write(favoriteFood);
+let pokemonList = [
+{name: "Butterfree", height: 1.1, types: ["flying", "bug"]},
+{name: "Arbok", height: 3.5, types: ["dark", "poison"]},
+{name: "Parasect", height: 1, types: ["grass", "bug"]}];
