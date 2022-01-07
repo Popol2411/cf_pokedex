6d54f0ecf0d119a06c9e05ffb2c912803540ad8a
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-01-07 22:22:04 +0100

    Indentation updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let pokemonList = [
-{name: "Butterfree", height: 1.1, types: ["flying", "bug"]},
-{name: "Arbok", height: 3.5, types: ["dark", "poison"]},
-{name: "Parasect", height: 1, types: ["grass", "bug"]}];
+    {name: "Butterfree", height: 1.1, types: ["flying", "bug"] },
+    {name: "Arbok", height: 3.5, types: ["dark", "poison"] },
+    {name: "Parasect", height: 1, types: ["grass", "bug"] }
+  ];
