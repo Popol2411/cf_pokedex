0d131f0490998dda4d82c7d0db51e47efcebd917
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-01-08 21:11:20 +0100

    indentation updated, for loop conditions added

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
 let pokemonList = [
-    {name: "Butterfree", height: 1.1, types: ["flying", "bug"] },
-    {name: "Arbok", height: 3.5, types: ["dark", "poison"] },
-    {name: "Parasect", height: 1, types: ["grass", "bug"] }
-  ];
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") ");
+  {
+    name: "Butterfree",
+    height: 1.1,
+    types: ["flying", "bug"]
+  },
+  {
+      name: "Arbok",
+      height: 3.5,
+      types: ["dark", "poison"]
+  },
+  {
+      name: "Parasect",
+      height: 0.8,
+      types: ["grass", "bug"]
+  }
+];
+
+for (let i = 0; i < pokemonList.length; i++){
+if (pokemonList[i].height >1.0) {
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that´s big!" + "<br><br>");
+}
+else {
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br><br>");
+}
 }
