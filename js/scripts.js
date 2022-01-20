36c8660deff5556693402ac3855d0a5c6875aba7
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-01-20 15:48:57 +0100

    comments added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-let pokemonRepository = (function () {
+let pokemonRepository = (function () { //function is anonymous since no parameter is given to it
 
-let pokemonList = [
+let pokemonList = [ //Array named "pokemonList" with the properties of 3 pokemons
   {
       name: "Butterfree",
       height: 1.1,
@@ -18,26 +18,26 @@ let pokemonList = [
   }
 ];
 
-  function add(pokemon) {
+  function add(pokemon) { // "add" function adds pokemon to the "pokemonList" via the "push" function
     pokemonList.push(pokemon);
   }
 
-  function getAll() {
+  function getAll() { // function "getAll" that returns all the data from the "pokemonList"
     return pokemonList;
   }
 
   function addListItem(pokemon) {
-    let pokemonList = document.querySelector(".pokemon-list");
-    let listItem = document.createElement("li");
-    let button = document.createElement("button");
-    button.innerText = pokemon.name;
-    button.classList.add("my_button");
-    listItem.appendChild(button);
+    let pokemonList = document.querySelector(".pokemon-list"); // "pokemon-list" class from html (ul) is chosen via "let pokemonList"
+    let listItem = document.createElement("li"); // list created via createElement
+    let button = document.createElement("button"); // button created
+    button.innerText = pokemon.name; // button text/description is the "name" of the pokemon
+    button.classList.add("my_button"); // css "my_button" taken into account
+    listItem.appendChild(button); // button is added to the item list
     pokemonList.appendChild(listItem);
-    button.addEventListener("click", function (event) {
-    console.log(event)
-    console.log(pokemon)
-    showDetails(pokemon)
+    button.addEventListener("click", function (event) { // "Event" added to the "click" of the button
+    console.log(event)  // "event" is logged -> "pointer" (the click itself)
+    console.log(pokemon) // the pokemon including his properties is shown
+    showDetails(pokemon) // same as above, but "showDetails" also contains the text "showDetails function says hello"
   })
 };
 
