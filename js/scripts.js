b22621231865a5fd0b2a57f7bf451d575482c7cb
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-01-21 23:37:17 +0100

    pokemontList array delete and left blank

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,7 @@
 let pokemonRepository = (function () { //function is anonymous since no parameter is given to it
 
-let pokemonList = [ //Array named "pokemonList" with the properties of 3 pokemons
-  {
-      name: "Butterfree",
-      height: 1.1,
-      types: ["flying", "bug"]
-  },
-  {
-      name: "Arbok",
-      height: 3.5,
-      types: ["dark", "poison"]
-  },
-  {
-      name: "Parasect",
-      height: 0.8,
-      types: ["grass", "bug"]
-  }
-];
+let pokemonList = []; //own array deleted and replaced by empty array since pokemons will be loaded via api
+let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; //api with the source of the pokemons
 
   function add(pokemon) { // "add" function adds pokemon to the "pokemonList" via the "push" function
     pokemonList.push(pokemon);
@@ -28,36 +13,67 @@ let pokemonList = [ //Array named "pokemonList" with the properties of 3 pokemon
 
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list"); // "pokemon-list" class from html (ul) is chosen via "let pokemonList"
-    let listItem = document.createElement("li"); // list created via createElement
+    let listpokemon = document.createElement("li"); // list created via createElement
     let button = document.createElement("button"); // button created
     button.innerText = pokemon.name; // button text/description is the "name" of the pokemon
     button.classList.add("my_button"); // css "my_button" taken into account
-    listItem.appendChild(button); // button is added to the item list
-    pokemonList.appendChild(listItem);
+    listpokemon.appendChild(button); // button is added to the item list
+    pokemonList.appendChild(listpokemon);
     button.addEventListener("click", function (event) { // "Event" added to the "click" of the button
     console.log(event)  // "event" is logged -> "pointer" (the click itself)
     console.log(pokemon) // the pokemon including his properties is shown
     showDetails(pokemon) // same as above, but "showDetails" also contains the text "showDetails function says hello"
-  })
-};
+  });
+}
 
-  function showDetails(pokemon) {
-    console.log('showDetails function says hello', pokemon)
+function loadList() {   //loadList function
+    return fetch(apiUrl).then(function (response) { //returns the data fetched from apiURL link on top of the script. If the promise takes place...
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) { // ...then for each "item" from the "pokemon" variable the name and detailsUrl are beeing displayed
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon); //the pokemon is added
+        console.log(pokemon); //the pokemon is also logged in the console for check
+      });
+    }).catch(function (e) { //if the promise fails, function "e" takes place
+      console.error(e);
+    })
   }
+
+  function loadDetails(item) { //loadDetails function
+    let url = item.detailsUrl; //the "url" variable corresponds to the "item.detailsUrl" data fetched
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {  //if the promise takes place, following details will be fetched
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) { //otherwise the error will be displayed in the console
+      console.error(e);
+    });
+
+    function showDetails(item) {
+      pokemonRepository.loadDetails(item).then(function () { //details loaded from the repository
+      console.log(item);
+    });
+  }
+
 
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
     showDetails: showDetails
   };
 })();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: "Pikachu", height: "0.5", types: ["yellow", "thunder"] });
-console.log(pokemonRepository.getAll());
-
+pokemonRepository.loadList().then(function() {
 pokemonRepository.getAll().forEach(function(pokemon) {
 pokemonRepository.addListItem(pokemon);
-
+  });
 });
